7786e4e4eff11109ead5e1ff1e59c25f651555fe
gabrielvm98 <64874245+gabrielvm98@users.noreply.github.com>
2024-09-11 20:33:55 -0500

    Spring Init

---
+++ seguroscrud/src/test/java/com/example/seguroscrud/SeguroscrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seguroscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguroscrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
